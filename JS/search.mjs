3104e5e2dfd677e540e96eb1f0b7a05fae8d62e1
Craft <38158429+Craft485@users.noreply.github.com>
2022-03-28 23:03:12 -0400

    Add JS implementation|Git warns about LF to CRLF in package.json

---
+++ JS/search.mjs
@@ -0,0 +1,29 @@
+import { readFile, writeFile, readdir } from 'fs';
+
+readdir('../datasets', {encoding: 'utf-8'}, (err, files) => {
+    if (err) throw err
+    
+    files.forEach(file => {
+        console.log(file)
+        const finalWordList = []
+        let longestWord = { word: '', length: 0 }
+
+        readFile(`../datasets/${file}`, {encoding: 'utf-8'},  (err, data) => {
+            if (err) throw err
+            const words = data.split('\n')
+        
+            words.forEach(word => { 
+                word = word.replace(/\r/, '').trim()
+                const sortedWord = word.trim().split('').sort().join('')
+                if (word === sortedWord) { 
+                    // console.log(word)
+                    finalWordList.push(word)
+                    if (word.length > longestWord.length) longestWord = { word: word, length: word.length }
+                }
+            })
+
+            writeFile(`../out/${file.split('.')[0]}.JS.log`, finalWordList.join('\n') + `\n${longestWord.word + ' | ' + longestWord.length}`, (err) => { if (err) throw err })
+            console.log(`# of words searched: ${words.length} | # of alphabetical words: ${finalWordList.length} | Longest Word Data: ${JSON.stringify(longestWord)}`)
+        })
+    })
+})
