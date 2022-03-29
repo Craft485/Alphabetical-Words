9452e86a3519f63c5ff3c5aecc693e00c50e54eb
Craft <38158429+Craft485@users.noreply.github.com>
2022-03-28 23:10:15 -0400

    Cleanup JS

--- JS/search.mjs
+++ JS/search.mjs
@@ -4,7 +4,7 @@ readdir('../datasets', {encoding: 'utf-8'}, (err, files) => {
     if (err) throw err
     
     files.forEach(file => {
-        console.log(file)
+        console.info(`Searching "${file}"`)
         const finalWordList = []
         let longestWord = { word: '', length: 0 }
 
@@ -15,15 +15,14 @@ readdir('../datasets', {encoding: 'utf-8'}, (err, files) => {
             words.forEach(word => { 
                 word = word.replace(/\r/, '').trim()
                 const sortedWord = word.trim().split('').sort().join('')
-                if (word === sortedWord) { 
-                    // console.log(word)
+                if (word === sortedWord) {
                     finalWordList.push(word)
                     if (word.length > longestWord.length) longestWord = { word: word, length: word.length }
                 }
             })
 
             writeFile(`../out/${file.split('.')[0]}.JS.log`, finalWordList.join('\n') + `\n${longestWord.word + ' | ' + longestWord.length}`, (err) => { if (err) throw err })
-            console.log(`# of words searched: ${words.length} | # of alphabetical words: ${finalWordList.length} | Longest Word Data: ${JSON.stringify(longestWord)}`)
+            console.info(`Showing results for: ${file} \n - # of words searched: ${words.length} | # of alphabetical words: ${finalWordList.length} | Longest Word Data: ${JSON.stringify(longestWord)}`)
         })
     })
 })
